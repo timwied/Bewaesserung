182b87dd173a648663341f7f92fa285534acca94
mitchirhd <michaelreinhard1711@gmail.com>
2024-01-15 09:22:57 +0100

    Code aufgeräumt

--- ESP32 Application/src/main.cpp
+++ ESP32 Application/src/main.cpp
@@ -9,7 +9,6 @@ int PinMoisture = 34;
 int PinWater = 35;
 int PinRelay = 5;
 
-bool ActivatePump = false;
 int PumpTime = 5000;
 int PumpStartTime = 0;
 int PumpBreakTime = 10000;
@@ -29,7 +28,6 @@ void HandleGetData(AsyncWebServerRequest* request)
   String json = String("{\"waterLevel\":") + WaterLevel +
                 String(",\"humidity\":") + Moisture +
                 String(",\"pumpStartTime\":") + PumpStartTime +
-                String(",\"activatePump\":") + ActivatePump +
                 String(",\"millis\":") + millis() +
                 String("}");
   request->send(200, "application/json", json);
@@ -52,29 +50,8 @@ void startServer()
 
   ////////////// WEBSERVER ////////////////
   AsyncWebServer *server = new AsyncWebServer(80);
-
-  // definieren von handler von get requests
   server->on("/", HTTP_GET, HandleRoot);
   server->on("/getData", HTTP_GET, HandleGetData);
-
-  server->on(
-      "/data", HTTP_POST,
-      [](AsyncWebServerRequest *request)
-      {
-        request->send(400, "text/html", "<h1>Body missing</h1>");
-      },
-      NULL,
-      [](AsyncWebServerRequest *request, uint8_t *data, size_t len,
-         size_t index, size_t total)
-      {
-        if (len != total)
-        {
-          request->send(400, "text/html", "<h1>Paginagion kickt</h1>");
-          return;
-        }
-        // processing of stuff
-        request->send(200, "text/html", "<h1>Processed</h1>");
-      });
 
   server->begin();
 }
@@ -95,20 +72,13 @@ void readWaterLevel()
     NoWater = false;
 }
 
-void readMoisture()
-{
-  Moisture = analogRead(PinMoisture);
-}
-
 //--------------------------------------------------------------------
 //--------------------------------------------------------------------
 
 void setup()
 {
   Serial.begin(9600);
-
   setPins();
-
   SPIFFS.begin();
   startServer();
 }
@@ -116,17 +86,15 @@ void setup()
 void loop()
 {
   readWaterLevel();
-  readMoisture();
+  Moisture = analogRead(PinMoisture);
 
   if (Moisture < 2000 && millis() - PumpStartTime > PumpBreakTime)
   {
     PumpStartTime = millis();
     digitalWrite(PinRelay, HIGH);
-    ActivatePump = true;
   }
   else if (millis() - PumpStartTime > PumpTime)
   {
     digitalWrite(PinRelay, LOW);
-    ActivatePump = false;
   }
 }
